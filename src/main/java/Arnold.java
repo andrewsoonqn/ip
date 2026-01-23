116ef1d993c7c95c49b6ab8f9195ae6bfe03e9fd
Andrew Soon <andrewsoonqn@gmail.com>
2026-01-23 17:09:08 +0800

    Refactor message printing
    The code is cluttered with constant reuse of the line() method. Lines are printed before and after each message.
    
    Introduce a method for printing messages which handles styling like the horizontal lines and indents.

--- src/main/java/Arnold.java
+++ src/main/java/Arnold.java
@@ -8,38 +8,46 @@ public class Arnold {
         System.out.print("    ");
         System.out.println(text);
     }
+
     private static void line() {
         indentPrint("____________________________________________________________");
+    }
+
+    public static void printMessage(String text) {
+        line();
+        for (String line : text.split("\n")) {
+            indentPrint(line);
+        }
+        line();
     }
 
     // ---------------------------------------------
     // Chatbot commands
     // ---------------------------------------------
+
     private static void hi() {
-        indentPrint("Hello! I'm Arnold");
-        indentPrint("What can I do for you?");
+        String message = "Hello! I'm Arnold" + "\n" + "What can I do for you?";
+        printMessage(message);
     }
 
     private static void bye() {
-        indentPrint("Bye. Hope to see you again soon!");
+        printMessage("Bye. Hope to see you again soon!");
+    }
+
     }
 
     public static void main(String[] args) {
         // Scanner is used to get user input later
         Scanner scanner = new Scanner(System.in);
 
-        Arnold.line();
-        Arnold.hi();
-        Arnold.line();
+        hi();
 
         while (true) {
             String input = scanner.nextLine();
             String inputCommand = input.strip().toLowerCase();
             if (inputCommand.equals("bye")) {
-                Arnold.line();
-                Arnold.bye();
-                Arnold.line();
-                System.exit(0);
+                bye();
+                break;
             } else {
                 Arnold.line();
                 indentPrint(input);
