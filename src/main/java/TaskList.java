35804c24f888268f601496092834468fe8e1819c
Andrew Soon <andrewsoonqn@gmail.com>
2026-01-23 22:25:36 +0800

    Refactor to use Task objects for task management
    Introduce a Task class to encapsulate task data, state, behaviour.
    
    Update Arnold and TaskList to use Task objects instead of raw strings, improving code structure and extensibility.

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class TaskList {
     // This TaskList implements a Singleton pattern
     private static final TaskList instance = new TaskList();
-    private final List<String> tasks = new ArrayList<>();
+    private final List<Task> tasks = new ArrayList<>();
 
     private TaskList() {}
 
@@ -12,16 +12,16 @@ public class TaskList {
         return instance;
     }
 
-    public String addTask(String task) {
+    public String addTask(Task task) {
         tasks.add(task);
-        return "added: " + task;
+        return "added: " + task.getDescription();
     }
 
     public String listTasks() {
         StringBuilder taskListBuilder = new StringBuilder();
 
         int counter = 1;
-        for (String task : tasks) {
+        for (Task task : tasks) {
             // Format as "{counter}. {task}"
             // e.g. "1. my task"
             taskListBuilder.append(String.format("%d. %s", counter, task));
