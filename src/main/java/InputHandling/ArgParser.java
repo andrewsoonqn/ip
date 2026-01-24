718a96f05e0d5402c22c525ca2c792f238719118
Andrew Soon <andrewsoonqn@gmail.com>
2026-01-24 14:29:27 +0800

    Add ArgParser utility
    Introduce ArgParser class for parsing command arguments and flags.

---
+++ src/main/java/InputHandling/ArgParser.java
@@ -0,0 +1,36 @@
+package InputHandling;
+
+import java.util.Map;
+
+public class ArgParser {
+    private static final String FLAG_SIGNIFIER = "/";
+
+    public static String[] getCommandArgs(String text) {
+        String[] parts = text.split("\\s+", 2);
+
+        // "Pair"
+        return parts.length > 1 ? new String[]{parts[0], parts[1]} : new String[]{parts[0], ""};
+    }
+
+    public static Map<String, String> getFlags(String text) {
+        text = text.strip().toLowerCase();
+        Map<String, String> flags = new java.util.HashMap<>();
+        String[] parts = text.split(FLAG_SIGNIFIER);
+
+        // The first part will be the task
+        // The rest will be flags
+        String taskDescription = parts[0];
+        for (int i = 1; i < parts.length; i++) {
+            String[] keyValue = parts[i].split("\\s+", 2);
+            if (keyValue.length == 2) {
+                flags.put(keyValue[0], keyValue[1]);
+            }
+        }
+        return flags;
+    }
+
+    public static void main(String[] args) {
+        getFlags("task /flag1 value1 /flag2 value2");
+    }
+
+}
