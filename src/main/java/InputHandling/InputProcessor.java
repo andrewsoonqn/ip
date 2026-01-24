aa26b472845f000da022e9b71901c2a08e7b77c6
Andrew Soon <andrewsoonqn@gmail.com>
2026-01-24 09:35:27 +0800

    Add InputProcessor for command handling
    Introduces the InputProcessor class to handle user input commands using strategy patterns.

---
+++ src/main/java/InputHandling/InputProcessor.java
@@ -0,0 +1,40 @@
+package InputHandling;
+
+import Messaging.Messenger;
+import Tasks.TaskList;
+
+import java.util.Map;
+
+public class InputProcessor {
+    private Messenger msg;
+    private TaskList taskList;
+
+    public InputProcessor(Messenger msg, TaskList taskList) {
+        this.msg = msg;
+        this.taskList = taskList;
+    }
+
+    private static final Map<String, InputHandlingStrategy> commandStrategies = Map.of(
+            "bye", new ExitStrategy(),
+            "list", new ListStrategy()
+//            "mark", new MarkStrategy(),
+//            "unmark", new UnmarkStrategy()
+    );
+
+    public void processInput(String input) {
+        String[] commandParts = input.strip().toLowerCase().split(" ");
+        String command = commandParts[0];
+        String arg = commandParts.length > 1 ? commandParts[1] : null;
+
+        InputHandlingStrategy strategy = commandStrategies.get(command);
+        if (strategy != null) {
+            strategy.handleInput(arg, msg, taskList);
+        } else {
+            new DefaultStrategy().handleInput(input, msg, taskList);
+        }
+    }
+
+    public void processInput(InputHandlingStrategy strategy, String arg) {
+        strategy.handleInput(arg, msg, taskList);
+    }
+}
