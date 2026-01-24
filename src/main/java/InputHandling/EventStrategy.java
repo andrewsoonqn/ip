02dfe732d36a01ce65f8cbdcba4c262b20605323
Andrew Soon <andrewsoonqn@gmail.com>
2026-01-24 22:42:31 +0800

    Handle missing event start or end time
    Ensure 'from' and 'to' flags are provided when creating an Event. Throw ChatbotArgumentException if either is missing.

--- src/main/java/InputHandling/EventStrategy.java
+++ src/main/java/InputHandling/EventStrategy.java
@@ -1,5 +1,6 @@
 package InputHandling;
 
+import ChatbotExceptions.ChatbotArgumentException;
 import Tasks.Event;
 import Tasks.Task;
 
@@ -9,6 +10,16 @@ public class EventStrategy extends AddTaskStrategy {
     @Override
     protected Task getTask(String input) {
         Map<String, String> flagValues = ArgParser.getFlags(input);
+
+        String from = flagValues.get("from");
+        if (from == null) {
+            throw new ChatbotArgumentException("Event start time is mandatory.");
+        }
+
+        String to = flagValues.get("to");
+        if (to == null) {
+            throw new ChatbotArgumentException("Event end time is mandatory.");
+        }
 
         return new Event(flagValues.get("taskDescription"), flagValues.get("from"), flagValues.get("to"));
     }
