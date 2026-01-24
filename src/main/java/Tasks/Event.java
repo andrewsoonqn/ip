bb99dfdd04b66d2c8a145a76b77f174cc448697e
Andrew Soon <andrewsoonqn@gmail.com>
2026-01-24 12:44:20 +0800

    Add Event class

---
+++ src/main/java/Tasks/Event.java
@@ -0,0 +1,18 @@
+package Tasks;
+
+public class Event extends Task {
+    private String from;
+    private String to;
+
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(" (from: %s to: %s)", from, to);
+    }
+}
+
