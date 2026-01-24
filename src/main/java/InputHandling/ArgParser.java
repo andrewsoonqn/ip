f576e1d81713de22f6fefa094d4f5fe90f19ae0b
Andrew Soon <andrewsoonqn@gmail.com>
2026-01-24 15:56:22 +0800

    Tweak usage of strip and toLowerCase methods
    Task descriptions and flag arguments are unintentionally converted to lowercase. Whitespace between arguments might also remain.
    
    Fix by tweaking usage of strip and removing toLowerCase.

--- src/main/java/InputHandling/ArgParser.java
+++ src/main/java/InputHandling/ArgParser.java
@@ -13,19 +13,18 @@ public class ArgParser {
     }
 
     public static Map<String, String> getFlags(String text) {
-        text = text.strip().toLowerCase();
         Map<String, String> flags = new java.util.HashMap<>();
         String[] parts = text.split(FLAG_SIGNIFIER);
 
         // The first part will be the task
-        String taskDescription = parts[0];
+        String taskDescription = parts[0].strip();
         flags.put("taskDescription", taskDescription);
 
         // The rest will be flags
         for (int i = 1; i < parts.length; i++) {
             String[] keyValue = parts[i].split("\\s+", 2);
             if (keyValue.length == 2) {
-                flags.put(keyValue[0], keyValue[1]);
+                flags.put(keyValue[0].strip(), keyValue[1].strip());
             }
         }
         return flags;
