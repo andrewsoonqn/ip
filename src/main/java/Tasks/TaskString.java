a0acdfefd8b1c7d8323564b840353c6e7bac7cb5
Andrew Soon <andrewsoonqn@gmail.com>
2026-01-24 15:04:49 +0800

    Minor tweak to task string representation

--- src/main/java/Tasks/TaskString.java
+++ src/main/java/Tasks/TaskString.java
@@ -2,10 +2,10 @@ package Tasks;
 
 public class TaskString {
     public static String withId(Task task) {
-        return String.format("%d. %s", task.getId(), task);
+        return String.format("%d.%s", task.getId(), task);
     }
 
     public static String withoutId(Task task) {
-        return String.format("   %s", task);
+        return String.format("  %s", task);
     }
 }
