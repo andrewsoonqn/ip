4dd725db06e8e6a8cb6969655758cfc9e0cb2d17
Andrew Soon <andrewsoonqn@gmail.com>
2026-01-25 08:04:57 +0800

    Throw a more specific exception

--- src/main/java/InputHandling/DefaultStrategy.java
+++ src/main/java/InputHandling/DefaultStrategy.java
@@ -1,12 +1,13 @@
 package InputHandling;
 
 import ChatbotExceptions.ChatBotException;
+import ChatbotExceptions.NoSuchCommandException;
 import Messaging.Messenger;
 import Tasks.TaskList;
 
 public class DefaultStrategy implements InputHandlingStrategy {
     @Override
     public void handleInput(String input, Messenger msg, TaskList taskList) {
-        throw new ChatBotException("Sorry, I don't recognise that command!");
+        throw new NoSuchCommandException("Sorry, I don't recognise that command!");
     }
 }
