62ab1821b2ac14da2ac248afbbda952a8c25360a
Andrew Soon <andrewsoonqn@gmail.com>
2026-02-02 15:05:05 +0800

    Refactor Deadline to use LocalDateTime

--- src/main/java/Tasks/Deadline.java
+++ src/main/java/Tasks/Deadline.java
@@ -1,16 +1,21 @@
 package Tasks;
 
-public class Deadline extends Task {
-    private String by;
+import InputHandling.DateTimeParser;
 
-    public Deadline(String description, String by) {
+import java.time.LocalDateTime;
+
+public class Deadline extends Task {
+    private LocalDateTime by;
+
+    public Deadline(String description, LocalDateTime by) {
         super(description);
         this.by = by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + String.format(" (by: %s)", by);
+        return "[D]" + super.toString()
+                + String.format(" (by: %s)", DateTimeParser.formatDateTime(by));
     }
 
     @Override
@@ -20,6 +25,6 @@ public class Deadline extends Task {
 
     @Override
     public String asCommand() {
-        return String.format("deadline %s /by %s", getDescription(), by);
+        return String.format("deadline %s /by %s", getDescription(), DateTimeParser.formatDateTime(by));
     }
 }
