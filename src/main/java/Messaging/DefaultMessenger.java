c3f0e86e047ed031d0d9c379ca15ad00f190568d
Andrew Soon <andrewsoonqn@gmail.com>
2026-02-02 10:42:08 +0800

    Refactor Messenger
    Convert Messenger to an abstract class and move message printing logic to DefaultMessenger. Add NullMessenger as a no-op implementation. This improves extensibility and separates concerns for different messaging behaviours.

---
+++ src/main/java/Messaging/DefaultMessenger.java
@@ -0,0 +1,18 @@
+package Messaging;
+
+public class DefaultMessenger extends Messenger {
+    @Override
+    public void printMessage(String text) {
+        line();
+        for (String line : text.split("\n")) {
+            indentPrint(line);
+        }
+        line();
+    }
+
+    @Override
+    public void hi() {
+        String message = "Hello! I'm Arnold" + "\n" + "What can I do for you?";
+        printMessage(message);
+    }
+}
---
+++ src/main/java/Messaging/NullMessenger.java
@@ -0,0 +1,11 @@
+package Messaging;
+
+public class NullMessenger extends Messenger {
+    @Override
+    public void printMessage(String message) {
+    }
+
+    @Override
+    public void hi() {
+    }
+}
