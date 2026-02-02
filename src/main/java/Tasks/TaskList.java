741d0a8cff9b6dc51abd1d276c764788bef0fde8
Andrew Soon <andrewsoonqn@gmail.com>
2026-02-02 10:45:02 +0800

    Integrate data persistence into TaskList
    TaskList now loads tasks from persistent storage on creation and saves changes after adding, marking, or unmarking tasks.

--- src/main/java/Tasks/TaskList.java
+++ src/main/java/Tasks/TaskList.java
@@ -1,5 +1,9 @@
 package Tasks;
 
+import DataPersistence.DataLoader;
+import DataPersistence.DataPaths;
+import DataPersistence.DataSaver;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,8 +11,18 @@ import java.util.stream.Collectors;
 public class TaskList {
     private final List<Task> tasks = new ArrayList<>();
 
+    private TaskList() {
+    }
+
+    public static TaskList create() {
+        TaskList taskList = new TaskList();
+        DataLoader.loadData(taskList);
+        return taskList;
+    }
+
     public Task addTask(Task task) {
         tasks.add(task);
+        DataSaver.saveData(DataPaths.TASKS_FILE_PATH, this);
         return task;
     }
 
@@ -35,12 +49,14 @@ public class TaskList {
     public Task markTask(int which) {
         Task task = getTask(which);
         task.mark();
+        DataSaver.saveData(DataPaths.TASKS_FILE_PATH, this);
         return task;
     }
 
     public Task unmarkTask(int which) {
         Task task = getTask(which);
         task.unmark();
+        DataSaver.saveData(DataPaths.TASKS_FILE_PATH, this);
         return task;
     }
 
