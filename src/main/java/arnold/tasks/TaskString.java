51054de5a1a586c4e3d990ec931356eb0281b04b
Andrew Soon <andrewsoonqn@gmail.com>
2026-02-06 19:24:52 +0800

    TaskString: add listWithIndex utility method

--- src/main/java/arnold/tasks/TaskString.java
+++ src/main/java/arnold/tasks/TaskString.java
@@ -8,4 +8,18 @@ public class TaskString {
     public static String withoutIndex(Task task) {
         return String.format("  %s", task);
     }
+
+    public static String listWithIndex(List<Task> tasks) {
+        StringBuilder taskListBuilder = new StringBuilder();
+
+        for (int i = 0; i < tasks.size(); i++) {
+            Task task = tasks.get(i);
+            taskListBuilder.append(TaskString.withIndex(task, i + 1));
+
+            if (i < tasks.size() - 1) {
+                taskListBuilder.append("\n");
+            }
+        }
+        return taskListBuilder.toString();
+    }
 }
