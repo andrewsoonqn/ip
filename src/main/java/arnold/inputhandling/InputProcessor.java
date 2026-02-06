89f6b7de108ef3fef6b8e20e123646e51a344fb8
Andrew Soon <andrewsoonqn@gmail.com>
2026-02-06 23:03:19 +0800

    Reformat code

--- src/main/java/arnold/inputhandling/InputProcessor.java
+++ src/main/java/arnold/inputhandling/InputProcessor.java
@@ -6,26 +6,10 @@ import arnold.chatbotexceptions.ChatbotException;
 import arnold.messaging.Messenger;
 import arnold.tasks.TaskList;
 
-import java.util.Map;
-
 /**
  * Processes user input and executes commands using appropriate strategies.
  */
 public class InputProcessor {
-    private final Messenger msg;
-    private final TaskList taskList;
-
-    /**
-     * Initializes a new instance of the InputProcessor.
-     *
-     * @param msg The messenger to use for communication.
-     * @param taskList The task list to manage.
-     */
-    public InputProcessor(Messenger msg, TaskList taskList) {
-        this.msg = msg;
-        this.taskList = taskList;
-    }
-
     private static final Map<String, InputHandlingStrategy> commandStrategies = Map.of(
             "bye", new ExitStrategy(),
             "list", new ListStrategy(),
@@ -39,6 +23,12 @@ public class InputProcessor {
     private final Messenger msg;
     private final TaskList taskList;
 
+    /**
+     * Initializes a new instance of the InputProcessor.
+     *
+     * @param msg      The messenger to use for communication.
+     * @param taskList The task list to manage.
+     */
     public InputProcessor(Messenger msg, TaskList taskList) {
         this.msg = msg;
         this.taskList = taskList;
@@ -70,7 +60,7 @@ public class InputProcessor {
      * Processes user input using a specific strategy.
      *
      * @param strategy The strategy to use for processing.
-     * @param arg The argument for the strategy.
+     * @param arg      The argument for the strategy.
      */
     public void processInput(InputHandlingStrategy strategy, String arg) {
         strategy.handleInput(arg, msg, taskList);
