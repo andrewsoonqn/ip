67f7ec90459809618fc102b1dcbc30436e24b185
Andrew Soon <andrewsoonqn@gmail.com>
2026-02-14 22:42:04 +0800

    Create TaskIdStrategy.java

---
+++ src/main/java/arnold/inputhandling/strategies/taskcrudstrategies/TaskIdStrategy.java
@@ -0,0 +1,34 @@
+package arnold.inputhandling.strategies.taskcrudstrategies;
+
+import arnold.chatbotexceptions.ChatbotArgumentException;
+import arnold.inputhandling.CommandResult;
+import arnold.inputhandling.Messages;
+import arnold.inputhandling.strategies.InputHandlingStrategy;
+import arnold.inputhandling.parsing.ParsedCommand;
+import arnold.tasks.utils.TaskList;
+
+/**
+ * Abstract strategy for commands that operate on a single task identified by its ID.
+ * Handles the shared logic of parsing the task ID from the command description.
+ */
+public abstract class TaskIdStrategy implements InputHandlingStrategy {
+    @Override
+    public CommandResult handleInput(ParsedCommand command, TaskList taskList) {
+        int taskId;
+        try {
+            taskId = Integer.parseInt(command.getDescription());
+        } catch (NumberFormatException e) {
+            throw new ChatbotArgumentException(Messages.invalidTaskId());
+        }
+        return execute(taskId, taskList);
+    }
+
+    /**
+     * Executes the command on the task identified by the given ID.
+     *
+     * @param taskId The parsed task ID.
+     * @param taskList The task list to operate on.
+     * @return The command result.
+     */
+    protected abstract CommandResult execute(int taskId, TaskList taskList);
+}
