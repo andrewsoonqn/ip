4d43fce2de4e27496f8832a86f3d661e5a02da6e
Andrew Soon <andrewsoonqn@gmail.com>
2026-02-17 16:05:54 +0800

    Create StorageException

---
+++ src/main/java/arnold/chatbotexceptions/StorageException.java
@@ -0,0 +1,26 @@
+package arnold.chatbotexceptions;
+
+/**
+ * Exception thrown when a storage operation (save or load) fails.
+ */
+public class StorageException extends ChatbotException {
+    /**
+     * Creates a StorageException with the specified message.
+     *
+     * @param message The error message.
+     */
+    public StorageException(String message) {
+        super(message);
+    }
+
+    /**
+     * Creates a StorageException with the specified message and cause.
+     *
+     * @param message The error message.
+     * @param cause The underlying cause of the exception.
+     */
+    public StorageException(String message, Throwable cause) {
+        super(message);
+        initCause(cause);
+    }
+}
